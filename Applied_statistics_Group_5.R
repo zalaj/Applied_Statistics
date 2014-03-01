8b01474891565326e0afe984a187711ded1871a3
zalaj <zala.jamsek@gmail.com>
2014-03-01 16:20:19 +0100

    final
    Monte Carlo simulacija, p vrednost

--- Applied_statistics_Group_5.R
+++ Applied_statistics_Group_5.R
@@ -1,3 +1,6 @@
+library (lattice)
+library(Matrix)
+
 #The data of pacients
 #This are the data from seven patients each underwent three different methods of kidney
 #dialysis. The following values were obtained for weight change in kilograms between 
@@ -10,18 +13,73 @@ Treatment_C <- c(2.67,2.62,1.84,2.33,1.27,2.39,2.39)
 #Construction of the data frame for the data of patients
 Patient <- data.frame(Patients,Treatment_A,Treatment_B,Treatment_C)
 
-#The function T calculates a T statistics for given data frame X
-T <- function (X){
-  M <- (mean(X$Treatment_A) + mean(X$Treatment_B) + mean(X$Treatment_C))/3
-  T <- (mean(X$Treatment_A) - M)^2 + (mean(X$Treatment_B) - M)^2 + (mean(X$Treatment_C)- M)^2
+
+#The function S calculates T statsitsics for a given data frame
+
+T <- function(X){
+  T <-(7*(mean(X$Treatment_A)^2) + 7*(mean(X$Treatment_B)^2) + 7*(mean(X$Treatment_C))^2)
   return(T)
 }
 
-S <- function(X){
-  S <-mean(X$Treatment_A)^2 + mean(X$Treatment_B)^2 + mean(X$Treatment_C)^2
-  return(S)
+#The function T calculates a T statistics for given data frame X
+T_1 <- function (X){
+  M <- (mean(X$Treatment_A) + mean(X$Treatment_B) + mean(X$Treatment_C))/3
+  T_1<- (mean(X$Treatment_A) - M)^2 + (mean(X$Treatment_B) - M)^2 + (mean(X$Treatment_C)- M)^2
+  return(T_1)
 }
 
-m <- factorial(3)^7
+#We calculate T and S for our data frame Patient
+t<-T(Patient)
+t
+s <- T(Patient)
+s
 
-??permutation
+#
+m <- factorial(3)^7
+n <- factorial (21)
+m/n
+
+#because the value of m/n is too high we use Monte-Carlo simulation
+
+##Monte Carlo symulation for caluculating p-value
+#The input for the function:
+#X - in data matrix, B - the number of iterations of Monte Carlo method,
+#fun - the T statistics (in our case this are T() and, S())
+Monte_Carlo <- function(X, B, fun){
+  
+  t_value <- fun(X)
+  permutation_distr <- vector()
+  
+  for (i in 1:B){
+    #we make a new matrix by taking a permutation of each line
+    for (row in 1:7){
+      X[row,2:4]<- sample(as.matrix(X)[row,2:4])
+    }
+    permutation_distr <- c(permutation_distr, fun(X))
+  
+  }
+
+  #Calculation of p-value
+  ident <-0
+  t_bar <-mean(permutation_distr)
+  for (i in 1:B){
+    if (abs(permutation_distr[i]- t_bar) >= abs(t_value-t_bar)){
+      ident <- ident +1      
+    }
+  }
+  
+  p <- (1+ident)/(1+B)
+  print(ident)
+  print(p)
+  #we draw a histogram of permutation distribution
+  hist(permutation_distr, nclass =100, ylab = 'Density', xlab = 'T', main ='A histogram of a permutation distribution' )
+  abline(v = t_value, col = 2)  
+}
+
+#Monte Carlo for test stetistics T
+Monte_Carlo(Patient,9999,T)
+
+#Monte Carlo for test stetistics T
+Monte_Carlo(Patient,9999,T_1)
+
+
